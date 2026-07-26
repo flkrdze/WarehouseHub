107667cfbb50c59ff12ff220fe0dce52c601824f
flkrdze <tarasovruslan815@gmail.com>
2026-07-26 14:24:56 +0300

    Добавьте файлы проекта.

---
+++ src/WarehouseHub.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/", () => "WarehouseHub API");
+
+app.Run();
