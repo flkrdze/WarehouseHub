66b17dfb6357dbc9a96f7ee4225f142d573488da
flkrdze <tarasovruslan815@gmail.com>
2026-08-16 16:39:46 +0300

    fix(.gitignore) minor fix

---
+++ src/WarehouseHub.Application/Abstractions/Authentication/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseHub.Application.Abstractions.Authentication
+{
+    public interface ITokenService
+    {
+        string GenerateToken(Guid userId);
+    }
+}
---
+++ src/WarehouseHub.Infrastructure/Authentication/JwtTokenService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseHub.Application.Abstractions.Authentication;
+
+namespace WarehouseHub.Infrastructure.Authentication
+{
+    public class JwtTokenService : ITokenService
+    {
+        public string GenerateToken(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
